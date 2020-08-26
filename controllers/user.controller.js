64a39963e27993f129c796aa6ecf7c9741dd6e97
PriyashanJayasankha <priyashanshell@gmail.com>
2020-08-26 11:36:20 +0530

    some changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,14 +4,15 @@ const passport = require("passport");
 
 module.exports.register = (req, res, next) => {
   var user = new User();
-  // (user.email = req.body.email), (user.email = req.body.email);
   user.email = req.body.email;
   user.password = req.body.password;
   user.name = req.body.name;
   user.nic = req.body.nic;
-  user.mobileNo = req.body.mobileNo,
+  user.mobileNo = req.body.mobileNo;
+
   user.save((err, doc) => {
     if (err) {
+      console.log("xxx");
       console.log("register error: " + JSON.stringify(err, undefined, 2));
       if (err.code == 11000) {
         res.status(422).send(["Duplicate email found"]);
--- models/user.model.js
+++ models/user.model.js
@@ -1,65 +1,59 @@
-const mongoose = require('mongoose');
-const bcrypt =  require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 var userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        unique:true,
-    },
-    mobileNo: {
-        type: Number,
-        
-    },
-    password: {
-        type: String
-    },
-    name: {
-        type: String
-    },
-    profilePicture: {
-        type: String
-    },
-    nic: {
-        type: Number,
-        
-    },
-    saltSecret: {
-        type: String
-    }
+  email: {
+    type: String,
+    unique: true,
+  },
+  mobileNo: {
+    type: Number,
+  },
+  password: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+  },
+  nic: {
+    type: String,
+  },
+  saltSecret: {
+    type: String,
+  },
 });
 
-
-userSchema.pre('save', function(next) {
-    bcrypt.genSalt(10, (err, salt) => {
-        if(err) {
-            console.log(err);
+userSchema.pre("save", function (next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.log(err);
+    } else {
+      bcrypt.hash(this.password, salt, (err, hash) => {
+        if (err) {
+          console.log(err);
         } else {
-            bcrypt.hash(this.password, salt, (err, hash) => {
-                if(err){
-                    console.log(err);
-                } else {
-                    this.password = hash;
-                    this.saltSecret = salt;
-                    next();
-                }
-            });
+          this.password = hash;
+          this.saltSecret = salt;
+          next();
         }
-    });
+      });
+    }
+  });
 });
-
 
 // Methods
 userSchema.methods.verifyPassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
-  
-userSchema.methods.generateJwt = function () {
-    return jwt.sign(
-           {_id: this._id},
-           process.env.JWT_SECRET,
-           {expiresIn: process.env.JWT_EXP}
-    );
-}
 
-mongoose.model('User', userSchema);
+userSchema.methods.generateJwt = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  });
+};
+
+mongoose.model("User", userSchema);
