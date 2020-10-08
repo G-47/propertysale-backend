ba228fc87ad3bbdf3feae28a810a910836b1a176
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-10-08 22:48:10 +0530

    report genarator updated

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -1,10 +1,39 @@
-const pdfDoc = require("pdfkit");
+const pdf = require("pdf-creator-node");
 const fs = require("fs");
 
-const doc = new pdfDoc();
+var html = fs.readFileSync("../template.html", "utf8");
+
+var options = {
+  format: "A3",
+  orientation: "portrait",
+  border: "10mm",
+  header: {
+    height: "45mm",
+    contents: '<div style="text-align: center;">Report</div>',
+  },
+  footer: {
+    height: "28mm",
+    contents: {
+      first: "Cover page",
+      2: "Second page",
+      default:
+        '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>',
+      last: "Last Page",
+    },
+  },
+};
 
 module.exports.create = (req, res) => {
-  doc.pipe(fs.createWriteStream("repot.pdf"));
-  doc.fontSize(15).text(req.body.content);
-  doc.end();
+  var document = {
+    html: html,
+    data: {
+      desc: req.body.content,
+    },
+    path: "../output.pdf",
+  };
+
+  pdf.create(document, options);
+  var data = fs.readFileSync("../output.pdf");
+  res.contentType("application/pdf");
+  res.send(data);
 };
