d758b6185826de25e70303702d57ce12f330e240
sandunsameera <sandunsameera25@gmail.com>
2020-10-10 00:35:11 +0530

    get request activity log

--- routes/index.router.js
+++ routes/index.router.js
@@ -13,52 +13,25 @@ const { verify } = require("jsonwebtoken");
 // USER CONTROLLERS
 router.post("/registerUser", ctrlUser.register);
 router.post("/authenticateUser", ctrlUser.authenticate);
-router.get(
-  "/getCurrentUser",
-  jwtHelper.verifyJwtToken,
-  ctrlUser.getCurrentUser
-);
+router.get("/getCurrentUser",jwtHelper.verifyJwtToken,ctrlUser.getCurrentUser);
 router.post("/getUser", jwtHelper.verifyJwtToken, ctrlUser.getUser);
 router.post("/getUsers", jwtHelper.verifyJwtToken, ctrlUser.getUsers);
 
 // DIRECT LAND/HOUSE CONTROLLERS
 router.post("/getDirectLands", ctrlDirectLand.allDirectLands);
-router.post(
-  "/addDirectLand",
-  jwtHelper.verifyJwtToken,
-  ctrlDirectLand.addDirectLand
-);
-router.put(
-  "/acceptDirectLand",
-  jwtHelper.verifyJwtToken,
-  ctrlDirectLand.acceptDirectLand
-);
+router.post("/addDirectLand",jwtHelper.verifyJwtToken,ctrlDirectLand.addDirectLand);
+router.put("/acceptDirectLand",jwtHelper.verifyJwtToken,ctrlDirectLand.acceptDirectLand);
 
 router.post("/getDirectHouses", ctrlDirectHouse.allDirectHouses);
-router.post(
-  "/addDirectHouse",
-  jwtHelper.verifyJwtToken,
-  ctrlDirectHouse.addDirectHouse
-);
-router.put(
-  "/acceptDirectHouse",
-  jwtHelper.verifyJwtToken,
-  ctrlDirectHouse.acceptDirectHouse
-);
+router.post("/addDirectHouse",jwtHelper.verifyJwtToken,ctrlDirectHouse.addDirectHouse);
+router.put("/acceptDirectHouse",jwtHelper.verifyJwtToken,ctrlDirectHouse.acceptDirectHouse);
 
 //MANAGER CONTROLLERS
 router.get("/getAdmins", jwtHelper.verifyJwtToken, ctrlAdmin.allAdmins);
 router.post("/postMessage", jwtHelper.verifyJwtToken, ctrlAdmin.postMessage);
-router.post(
-  "/postAuctionProperty",
-  jwtHelper.verifyJwtToken,
-  ctrlAdmin.postAuctionProperty
-);
-router.delete(
-  "/removeAdmin/:id",
-  jwtHelper.verifyJwtToken,
-  ctrlAdmin.removeAdmin
-);
+router.post("/postAuctionProperty",jwtHelper.verifyJwtToken,ctrlAdmin.postAuctionProperty);
+router.delete("/removeAdmin/:id",jwtHelper.verifyJwtToken,ctrlAdmin.removeAdmin);
+router.get("/getActivityLog",jwtHelper.verifyJwtToken,ctrlAdmin.getActivityLog);
 
 //EMAIL CONTROLLER
 router.post("/sendEmail", ctrlEmail.send);
